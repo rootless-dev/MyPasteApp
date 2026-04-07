178d3721145fb193c2ff1e6423de2bcf49d95e1e
Carlos Eduardo <71237690+carlosealves2@users.noreply.github.com>
2026-04-07 09:00:25 -0300

    Initial Commit

---
+++ MyPasteApp/MyPasteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPasteAppApp.swift
+//  MyPasteApp
+//
+//  Created by Carlos Eduardo on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyPasteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
